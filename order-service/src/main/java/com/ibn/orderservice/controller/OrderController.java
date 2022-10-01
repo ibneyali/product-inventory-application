bebd02821738011b6c80a20e3098b3306f0f62cf
Ibney Ali <Ibney.Ali@hansencx.com>
2022-10-01 23:53:02 +0530

    Implement actuator and resilience4j properties

--- order-service/src/main/java/com/ibn/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/ibn/orderservice/controller/OrderController.java
@@ -3,12 +3,16 @@ package com.ibn.orderservice.controller;
 import com.ibn.orderservice.dto.OrderRequest;
 import com.ibn.orderservice.dto.OrderResponse;
 import com.ibn.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api")
@@ -18,12 +22,19 @@ public class OrderController {
     OrderService orderService;
     @RequestMapping(value = "/order", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        return orderService.saveOrder(orderRequest);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(()-> orderService.saveOrder(orderRequest));
     }
     @RequestMapping(value = "/order", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public List<OrderResponse> getAllOrders(){
         return orderService.getAllOrders();
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Something went wrong");
     }
 }
